e5b35ed150ce6a868d5bca29704c7252f5b9da2c
DiegoRodriguez1203 <diegorodriguez1203@gmail.com>
2023-01-06 21:04:06 -0500

    app.js functional

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import { Start } from './components/Start'
 
-import React, { useEffect, useState } from 'react';
+import {Sectiondepilacion} from './pages/sections/sectiondepilacion'
+import {Sectionfacial} from './pages/sections/sectionfacial'
+import {Sectionspa} from './pages/sections/sectionspa'
+import { Sectionmasaje } from './pages/sections/sectionmasaje';
+
+import { Cartmarket } from './components/Cartmarket';
+
+import { GeneralProvider } from './context/GeneralContext';
 
 
 
 function App() {
   
   return (
-   <div>
-    <p>Hola mundo</p>
-   </div>
+    
+    <GeneralProvider>  {/*Context added from GeneralContext, called GeneralProvider */}
+    <section className="App">
+      <Routes> {/*Routes stablished in the app */}
+        <Route path='/' element = {<Start/>}/>
+
+        <Route path='/micarrito' element = {<Cartmarket/>}/>
+        <Route path='/productos' element = {<Sectionmasaje/>}/>
+        <Route path='/productos/masajes' element = {<Sectionmasaje/>}/>
+        <Route path='/productos/facial' element = {<Sectionfacial/>}/>
+        <Route path='/productos/depilacion' element = {<Sectiondepilacion/>}/>
+        <Route path='/productos/spa' element = {<Sectionspa/>}/>
+      </Routes>
+    </section>
+    </GeneralProvider>
   );
 }
 
