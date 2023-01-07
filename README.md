1dcc33f322221744a228e0dcbd3abfe2b035714d
DiegoRodriguez1203 <diegorodriguez1203@gmail.com>
2023-01-06 19:48:42 -0500

    databes and imgs

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-Sahury Spa - App web  
+Sahury Spa - Web APP  
+
+This is a web app made by Elopipe for Sahury Spa, a small company, located at Bogota - Colombia, which principal business activity it´s to bring spa, skincare, massages, and other beauty services to its consumers.
+
+To give a more professional service, this web app will allow its users (sahury spa's costumers) saw different kind of services, divided by categories, with it owns descriptions, prices, images and add this to a cart market.
+
+Database -- 
+For this proyect, a small array was created with categorys (4) and each category will have an array with it products. It is saved at the --- database.js --- document.
---
+++ src/components/database.js
@@ -0,0 +1,140 @@
+export const section = [
+    {name: 'Masajes',
+    img: 0},
+    {name: 'Facial',
+    img: 1},
+    {name: 'Depilación',
+    img: 2},
+    {name: 'Spa',
+    img: 3}
+]
+
+
+export const cardfacial = [
+
+    {name: 'Limpieza facial',
+    description: 'Técnica para mejorar la salud y la apariencia del cutis, este tratamiento elimina los puntos negros y las células muertas, con lo que se consigue que la piel respire y absorba mejor los tratamientos cosméticos o de medicina estética.',
+    price:34990,
+    id: 0,
+    item: 6, 
+    category:'facial'},
+
+    {name: 'Hidratación facial',
+    description: 'Es un tratamiento muy efectivo para nutrir, renovar e hidratar la piel. El resultado es un rostro más luminoso y con una textura suave.',
+    price:34990,
+    id: 1,
+    item: 7, 
+    category:'facial'},
+
+    {name: 'Plasma rico en plaquetas',
+    description: 'Actúa incrementando el grosor de la capa más superficial de la piel, mejorando el aspecto de arrugas, cicatrices y estrías. Además, estimula las células dérmicas y reorganiza las fibras de colágeno y elastina.',
+    price:59990,
+    id: 2,
+    item: 8, 
+    category:'facial'},
+
+    {name: 'Despigmentación facial',
+    description: 'Procedimiento medico-estético, para solucionar el problema de la hiper-pigmentación de la piel (coloración oscura de la cara que aparece lentamente). Común en personas de más de 30 años y en ancianos',
+    price:59990,
+    id: 3,
+    item: 9, 
+    category:'facial'},
+
+    {name: 'Dermapen Facial',
+    description: 'Es un dispositivo que consta de 11 microagujas que efectúan micropunciones sobre la piel produciendo la estimulación de los fibroblastos, encargados de la producción de colágeno y elastina, que sirve para reafirmar y rejuvenecer la piel.',
+    price:59990,
+    id: 4,
+    item: 10, 
+    category:'facial'}
+
+]
+
+export const cardcorporal = [
+
+    {name: 'Masaje corporal',
+    description: 'el tratamiento terapéutico más antiguo y la manera más natural y sencilla de combatir dolencias relacionadas con la musculatura, los hábitos posturales y los problemas circulatorios, entre otros.',
+    price:99900,
+    id: 0,
+    item: 1, 
+    category:'corporal'},
+
+    {name: 'Masaje post-operatorio',
+    description: 'Toda persona luego de realizarse una intervención quirúrgica pretende obtener los mejores resultados posibles y conseguir resolver esa inquietud estética que afecta su salud, tanto física o psicológica. Para resolver esto el masaje post-operatorio es ideal',
+    price:99900,
+    id: 1,
+    item: 2, 
+    category:'corporal'},
+
+    {name: 'Masaje relajante',
+    description: 'Consiste en la realización de maniobras superficiales en las que la intensidad de la presión es suave y el ritmo lento y reiterativo, de manera que al recibir un contacto repetido y constante, se pierde la sensación de dolor y los músculos se relajan.',
+    price:99900,
+    id: 2,
+    item: 3, 
+    category:'corporal'},
+
+    {name: 'Masaje descontracturante',
+    description: 'Es un tipo de masaje que tiene como objetivo liberar los músculos y las articulaciones para ayudarlos a funcionar en su nivel óptimo. El propósito del masaje es relajar y aflojar los músculos rígidos y tensos.',
+    price:99900,
+    id: 3,
+    item: 4, 
+    category:'corporal'},
+
+    {name: 'Drenaje linfático',
+    description: 'Es uno de los cuidados esenciales que otorga mayor efectividad a la hora de conseguir recuperarse con éxito de la cirugía y, por consiguiente, obtener unos resultados realmente satisfactorios.',
+    price:99900,
+    id: 4,
+    item: 5, 
+    category:'corporal'}
+
+]
+
+export const carddepilacion = [
+
+    {name: 'Depilación general',
+    description: 'Procedimiento cosmético que se basa en la eliminación del vello, ubicado en todas zonas del cuerpo. Se efectúa con emisión local de rayos X, esto hace que se deteriore la raíz e impulse la caída del vello.',
+    price:169900,
+    id: 0,
+    item: 11, 
+    category:'depilacion'}
+
+]
+
+export const cardspa = [
+
+    {name: 'Coco - Summer',
+    description: 'Disfruten nuestros Rituales spa para parejas. que incluyen los más selectos tratamientos. Todos irán acompañados de decoración con pétalos de rosas y velas junto a una ambientación especial con aromaterapia y musicoterapia. ',
+    price:359900,
+    id: 0,
+    item: 12, 
+    category:'spa'},
+
+    {name: 'Baño de luna',
+    subname: 'Limpia tu cara',
+    description: 'En un momento tan único como tu matrimonio preparamos un ritual muy especial para ti en donde mimaremos tus sentidos y quitaremos un poco las ansias que puedes tener de este anhelado día.',
+    price:259900,
+    id: 1,
+    item: 13, 
+    category:'spa'},
+
+    {name: 'Volcanoterapia',
+    description: 'El masaje con piedras volcánicas trabaja sobre el dolor de los músculos y estimula una sensación de relajación general.Las piedras calientes ejercen sobre el cuerpo un alivio que facilita el flujo de la energía vital.',
+    price:189900,
+    id: 2,
+    item: 14, 
+    category:'spa'},
+
+    {name: 'Lodoterapia',
+    description: 'Es una técnica milenaria donde el cuerpo es recubierto totalmente (excepto la cabeza) con barro natrutal diluido con agua termal, el barro es extraído de las profundidades del mar muerto.',
+    price:129900,
+    id: 3,
+    item: 15, 
+    category:'spa'},
+
+    {name: 'Tarde de amigas',
+    description: 'Sal de la rutina… Relájate y disfruta un plan spa para amigos con una experiencia única de relajación acompañado de una buena botella de vino. Celebra despedidas de soltera, cumpleaños u ocasiones especiales con tus amigas.',
+    price:424900,
+    id: 4,
+    item: 16, 
+    category:'spa'},
+]
+
